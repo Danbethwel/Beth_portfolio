cfe1f76121cbdd5f845def5194228d1458d46a85
Danbethwel <93147941+Danbethwel@users.noreply.github.com>
2023-03-29 22:29:46 +0300

    add

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "~": path.resolve(__dirname, "node_modules"),
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-  build: {
-    chunkSizeWarningLimit: 1600,
-  },
-});
+})
